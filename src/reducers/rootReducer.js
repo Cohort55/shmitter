549ed13d4130391fec8a49aa39caf40513eb84fd
edward <edwardlfish@gmail.com>
2025-04-08 13:47:49 +0300

    statsSlice with changeStats bug

--- src/reducers/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import {userReducer} from "./userReducer.js";
-import {statsReducer} from "./statsReducer.js";
-
-export const rootReducer = (state, action) => {
-    return {
-        user: userReducer(state.user, action),
-        stats: statsReducer(state.stats, action)
-    }
-}
